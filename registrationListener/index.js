6277b6684c66303c6746b697ed0c29f28a1ae029
ssskram <paul.marks@pittsburghpa.gov>
2019-02-21 09:51:54 -0500

    first test http call from sp table

--- registrationListener/index.js
+++ registrationListener/index.js
@@ -1,26 +1,20 @@
 module.exports = (context, req) => {
-    context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl)
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        }
-    } else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        }
-    }
-    context.done()
+  console.log(req.body)
+  console.log(req.query.type)
+  context.res = {
+    // status defaults to 200 */
+    body: 'Hello'
+  }
+  context.done()
 
-    // test test
-    // http endpoint
-    // takes a registration record in the body
-    // and an activity type in as a query param, "Update" || "New"
+  // test test
+  // http endpoint
+  // takes a registration record in the body
+  // and an activity type in as a query param, "Update" || "New"
 
-    // if activity type == "New" && registration status == "Active", email registration confirmation
-    // if activity type == "New" && registration status == "Waitlisted", email waitlist confirmation
-    // if activity type == "Update" && registration status == "Active", email registration confirmation
-    // if activity type == "Update" && registration status == "Canceled", email cancellation confirmation
+// if activity type == "New" && registration status == "Active", email registration confirmation
+// if activity type == "New" && registration status == "Waitlisted", email waitlist confirmation
+// if activity type == "Update" && registration status == "Active", email registration confirmation
+// if activity type == "Update" && registration status == "Canceled", email cancellation confirmation
 }
