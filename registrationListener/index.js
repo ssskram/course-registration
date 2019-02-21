f3322f94a216ade34e0f7a8ba0c26f85442991cb
ssskram <paul.marks@pittsburghpa.gov>
2019-02-21 11:06:48 -0500

    sending npm modules to production, per docs

--- registrationListener/index.js
+++ registrationListener/index.js
@@ -1,20 +1,36 @@
-module.exports = (context, req) => {
+const fetch = require('node-fetch')
+global.Headers = fetch.Headers
 
-  console.log(req.body)
-  console.log(req.query.type)
-  context.res = {
-    // status defaults to 200 */
-    body: 'Hello'
-  }
-  context.done()
+module.exports = async (context, req) => {
 
-  // test test
-  // http endpoint
-  // takes a registration record in the body
-  // and an activity type in as a query param, "Update" || "New"
+    // get courses here
+    const activity = req.query.type
+    const registrationType = req.body.registrationType
+    const course = await fetch("https://365proxy.azurewebsites.us/iphelp/course?courseCode=" + req.body.courseCode, {
+        method: 'get',
+        headers: new Headers({
+            'Authorization': 'Bearer ' + process.env.APP_365_API
+        })
+    })
+        .then(res => res.json())
+        .then(data => data)
 
-// if activity type == "New" && registration status == "Active", email registration confirmation
-// if activity type == "New" && registration status == "Waitlisted", email waitlist confirmation
-// if activity type == "Update" && registration status == "Active", email registration confirmation
-// if activity type == "Update" && registration status == "Canceled", email cancellation confirmation
+    if (activity == "new") {
+        if (registrationType == "Active") {
+
+        }
+        if (registrationType == "Waitlisted") {
+
+        }
+    }
+    if (activity == "update") {
+        if (registrationType == "Active") {
+
+        }
+        if (registrationType == "Canceled") {
+
+        }
+    }
+
+    context.done()
 }
