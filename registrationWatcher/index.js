bfba4070818bf0e60d70bbdec9d6140ba086762f
ssskram <paul.marks@pittsburghpa.gov>
2019-02-22 12:26:58 -0500

    building watcher, testing in prod

--- registrationWatcher/index.js
+++ registrationWatcher/index.js
@@ -1,6 +1,15 @@
+const fetch = require('node-fetch')
+global.Headers = fetch.Headers
+
 module.exports = async (context, timer) => {
-    // runs every night
+
+    // this runs every night
     const now = new Date()
+
+    /* 
+        First, try to find courses where a waitlist exists, but the maximum capacity has not been reached
+        For each one of these, take the oldest n number of waitlisted registrations, and set them to active
+    */
 
     // get all upcoming courses
     const courses = await fetch('https://365proxy.azurewebsites.us/iphelp/allCourses', {
@@ -12,15 +21,40 @@ module.exports = async (context, timer) => {
         .then(res => res.json())
         .then(data => data.filter(course => new Date(course.startDate) > now))
 
-    context.log(courses)
+    // get all registrations
+    const registrations = await fetch('https://365proxy.azurewebsites.us/iphelp/allCourseRegistrations', {
+            method: 'GET',
+            headers: new Headers({
+                'Authorization': 'Bearer ' + process.env.APP_365_API
+            })
+        })
+        .then(res => res.json())
+        .then(data => data)
 
-    // get table of registrations
-    // for each course, if maximum capacity is not reached, but waitlisted registration exist
-    // bump the oldest wait listed registrations up to fill the course
+    // for each course, if space is available and waitlist exists, bump oldest waitlisted enrollments to active
+    courses.forEach(course => {
+        const maxCapacity = course.maximumCapacity
+        const allEnrollments = registrations.filter(reg => reg.courseCode == course.courseCode)
+        const activeEnrollments = allEnrollments.filter(reg => reg.registrationStatus == "Active")
+        const waitlistedEnrollments = allEnrollments.filter(reg => reg.registrationStatus == "Waitlisted")
+        if (activeEnrollments < maxCapacity) {
+            if (waitlistedEnrollments.length > 0) {
+                const spotsToFill = maxCapacity - activeEnrollments
+                const sortedByDateSubmitted = waitlistedEnrollments.sort((a, b) => new Date(a.startDate) - new Date(b.startDate))
+                const registrationsToBump = sortedByDateSubmitted.slice(0, spotsToFill)
+                registrationsToBump.forEach(registration => await setToActive(registration))
+            } else return
+        } else return
+    })
 
-    // then, if any courses are occuring within 48 hours
-    // get the list of fully registered users
-    // and email them a reminder
+    /* 
+        Next, find courses that are occuring between 24 & 48 hours in the future
+        For each one of these course, email all registered users, where status == "Active", and remind them of the event
+    */
 
     context.done()
+
+    const setToActive = (registration) => {
+        context.log(registration)
+    }
 }
