888ac6d04f51d04f63bf0797c7001ba9feb7ccf2
ssskram <paul.marks@pittsburghpa.gov>
2019-02-22 12:34:47 -0500

    checking date sort on users to by bumped

--- registrationWatcher/index.js
+++ registrationWatcher/index.js
@@ -46,7 +46,7 @@ module.exports = async (context, timer) => {
                 const sortedByDateSubmitted = waitlistedEnrollments.sort((a, b) => new Date(a.startDate) - new Date(b.startDate))
                 const registrationsToBump = sortedByDateSubmitted.slice(0, spotsToFill)
                 registrationsToBump.forEach(registration => {
-                    context.log("To date: " + registration)
+                    context.log("To bump: " + registration.user)
                     // setToActive(registration)
                 })
             } else return
