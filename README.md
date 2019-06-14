a2e4c992f32fa3156ca7cc9cf45d4eda6466b484
ssskram <paul.marks@pittsburghpa.gov>
2019-06-14 12:07:48 -0400

    prettify, docs

---
+++ README.md
@@ -0,0 +1,38 @@
+# Course Registration
+
+Course Registration is a BaaS service hosted in Azure...
+
+## cancelRegistration
+ß
+## registrationListener
+
+## registrationWatcher
+
+## Authorization
+
+Keys! For the http triggers.
+
+## Running Locally
+
+### Prerequisites
+* [.Net Core](https://dotnet.microsoft.com/download) - BaaS execution environment
+* [Node.js](https://nodejs.org) - JS runtime
+* local.settings.json - See local.settings.json.example for all required secrets
+
+### Installation
+```
+git clone https://github.com/CityofPittsburgh/course-registration
+cd course-registration
+func extensions install
+func host start
+```
+
+## Deployment
+
+Deployed as an Azure Function.  Application is deployed directly from github, and can be triggered either (a) through the Azure GUI, (b) through the [CLI](https://docs.microsoft.com/en-us/cli/azure/webapp/deployment/source?view=azure-cli-latest#az-webapp-deployment-source-sync), or (c) through the [proxy service](https://github.com/CityofPittsburgh/azure-proxy).
+
+For complete documentation on the azure environment, see [here](https://github.com/CityofPittsburgh/all-things-azure.git).
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- cancelRegistration/index.js
+++ cancelRegistration/index.js
@@ -1,51 +1,65 @@
-const fetch = require('node-fetch')
-global.Headers = fetch.Headers
+const fetch = require("node-fetch");
+global.Headers = fetch.Headers;
 
 module.exports = async (context, req) => {
+  // return cancelation confirmation
+  context.res = {
+    status: 202,
+    headers: {
+      "Content-Type": "text/html"
+    },
+    body:
+      "<html><body><h1>You've successfully canceled your course enrollment</h1><h3>Thanks for letting us know!!</h3></body></html>"
+  };
 
-    // return cancelation confirmation
-    context.res = {
-        status: 202,
-        headers: {
-            "Content-Type": "text/html"
-        },
-        body: "<html><body><h1>You've successfully canceled your course enrollment</h1><h3>Thanks for letting us know!!</h3></body></html>",
+  // incoming params
+  const registrationId = req.query.id;
+  const user = req.query.user;
+
+  // set registration status == "Canceled"
+  fetch(
+    "https://365proxy.azurewebsites.us/iphelp/updateCourseRegistration?id=" +
+      registrationId,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        Registration_x0020_Status: "Canceled"
+      }),
+      headers: new Headers({
+        Authorization: "Bearer " + process.env.APP_365_API,
+        "Content-type": "application/json"
+      })
     }
+  );
 
-    // incoming params
-    const registrationId = req.query.id
-    const user = req.query.user
-
-    // set registration status == "Canceled"
-    fetch('https://365proxy.azurewebsites.us/iphelp/updateCourseRegistration?id=' + registrationId, {
-        method: 'POST',
-        body: JSON.stringify({
-            "Registration_x0020_Status": "Canceled"
-        }),
+  try {
+    // get calendar event
+    const eventId = await fetch(
+      "https://365proxy.azurewebsites.us/iphelp/courseRegistrationCalendarEvent?registrationID=" +
+        registrationId,
+      {
+        method: "GET",
         headers: new Headers({
-            'Authorization': 'Bearer ' + process.env.APP_365_API,
-            'Content-type': 'application/json'
+          Authorization: "Bearer " + process.env.APP_365_API
         })
-    })
+      }
+    )
+      .then(res => res.json())
+      .then(data => data[0].eventId);
 
-    try {
-        // get calendar event
-        const eventId = await fetch('https://365proxy.azurewebsites.us/iphelp/courseRegistrationCalendarEvent?registrationID=' + registrationId, {
-                method: 'GET',
-                headers: new Headers({
-                    'Authorization': 'Bearer ' + process.env.APP_365_API
-                })
-            })
-            .then(res => res.json())
-            .then(data => data[0].eventId)
-
-        // delete calendar event
-        fetch('https://365proxy.azurewebsites.us/calendar/deleteEvent?user=' + user + "&eventId=" + eventId, {
-            method: 'POST',
-            headers: new Headers({
-                'Authorization': 'Bearer ' + process.env.APP_365_API,
-                'Content-type': 'application/json'
-            })
+    // delete calendar event
+    fetch(
+      "https://365proxy.azurewebsites.us/calendar/deleteEvent?user=" +
+        user +
+        "&eventId=" +
+        eventId,
+      {
+        method: "POST",
+        headers: new Headers({
+          Authorization: "Bearer " + process.env.APP_365_API,
+          "Content-type": "application/json"
         })
-    } catch (err) {}
-}
+      }
+    );
+  } catch (err) {}
+};
